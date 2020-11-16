9b0c892bc2919b56df68756c7e72a6debde781fb
junchh <cjunho9868@gmail.com>
2020-11-16 17:16:18 +0700

    masih gajalan

---
+++ client.py
@@ -0,0 +1,79 @@
+import socket 
+import sys 
+import lib
+import time
+import random
+import pyaudio
+import wave
+
+#Input from user
+address = input("masukkan IP address server")
+port = int(input("masukkan port"))
+addr = (address, port)
+
+buffSize = 32767 + 7
+sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM) 
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+timeout = time.time() + 60*5
+
+subPacket = lib.createPacket("SUB", "")
+
+sampwidth = 0
+nchannel = 0
+framerate = 0
+
+run = True
+while(run):
+    sock.sendto(subPacket, addr)
+    print("menunggu...")
+    sock.settimeout(2)
+    try:
+        fromreceiver, _ = sock.recvfrom(buffSize)
+        typ, data = lib.breakPacket(fromreceiver)
+        print(typ)
+        print(data)
+        if(typ == "META"):
+            sampwidth = data[0]
+            nchannel = data[1]
+            framerate = data[2]
+            print(framerate)
+            run = False
+            break
+
+    except socket.timeout:
+        print("Timeout! Ulang")
+    except ConnectionResetError:
+        print("belom ada receiver, kirim ulang")
+        time.sleep(1)
+    if(time.time() > timeout):
+        break
+
+p = pyaudio.PyAudio()
+
+# Open a .Stream object to write the WAV file to
+# 'output = True' indicates that the sound will be played rather than recorded
+stream = p.open(format = p.get_format_from_width(sampwidth),
+                channels = nchannel,
+                rate = framerate,
+                output = True)
+
+queue = []
+
+while(True):
+    print("Hello from receiver")
+    try:
+        data, _ = sock.recvfrom(buffSize)
+        typ, payload = lib.breakPacket(data)
+
+        if(typ == 0x3):
+            queue.append(payload)
+    except:
+        print("error")
+
+    if(time.time() > timeout):
+        break
+
+for a in queue:
+    stream.write(a)
+
+
--- server.py
+++ server.py
@@ -9,7 +9,7 @@ import wave
 from threading import Thread
 
 buffSize = 32767 + 13
-chunkSize = 1024
+chunk = 1024
 subscribers = []
 timeout = time.time() + 60 * 5
 
@@ -24,6 +24,7 @@ def serverListener(receiver, metaPacket):
             typ, data = lib.breakPacket(message)
 
             if (typ == "SUB"):
+                print("sub baru nih")
                 receiver.sendto(metaPacket, addr)
                 subscribers.append(addr) 
 
@@ -33,6 +34,10 @@ def serverListener(receiver, metaPacket):
         if(time.time() > timeout):
             break
 
+def sendPacket(receiver, dataPacket, addr):
+
+    receiver.sendto(dataPacket, addr)
+
 
 
 
@@ -40,10 +45,13 @@ port = int(input("Masukkan port yang akan dibind: "))
 filename = input("Masukkan nama file WAV yang ingin dibroadcast ke seluruh client: ")
 
 
+
 #Open a socket connection and bind it to a port
 receiver = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 receiver.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 receiver.bind((socket.gethostbyname(socket.gethostname()), port))
+
+print(socket.gethostbyname(socket.gethostname()))
 
 # Open the WAV file
 wf = wave.open(filename, 'rb')
@@ -56,9 +64,14 @@ metaPacket = lib.createPacket("META", metadata)
 t = Thread(target = serverListener, args = (receiver, metaPacket))
 t.start()
 
-while(True):
-    print("notlis")
-    time.sleep(2)
+data = wf.readframes(chunk)
+
+while data != b'':
+    print("hehe")
+    dataPacket = lib.createPacket("DATA", data)
+    for addr in subscribers:
+        sendPacket(receiver, dataPacket, addr)
+    data = wf.readframes(chunk)
 
 
 
